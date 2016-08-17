fd47009ffff3676423a77957039366ddbe4558c4
Stefan Pijnappels <stefan@puppet.com>
2016-08-17 22:34:20 +0100

    first draft of the module

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # proxy_setup
-Tiny module to install and configure tinyproxy on a Linux host.
+Tiny Puppet module to install and configure tinyproxy on a Linux host.
+
+This module has one class, which, when applied, will install tinyproxy on either a Debian based Linux machine (Debian, Ubuntu and derivatives) or a RedHat based Linux machine (RHEL, CentOS, Oracle Linux and derivatives).
+It will create a simple config file and place it in the correct location. This config file will accept proxied requests from any IP on the proxy host's network as determined by facter. The CIDR subnet notation needs tidying up and will currently only work on /24 subnets. This will be addressed in a later version.
---
+++ manifests/init.pp
@@ -0,0 +1,41 @@
+# This is the init.pp for the proxy_setup module
+
+class proxy_setup (
+  # Create parameter and default value for port
+  $port = '8888',
+){
+  # Check for OS family to verify it's Linux and set the confdir
+  case $::osfamily {
+    'debian':{
+      $confdir = '/etc/tinyproxy.conf'
+    }
+    'redhat':{
+      # In RedHat we also need to install the Epel repo
+      $confdir = '/etc/tinyproxy/tinyproxy.conf'
+      package {'epel-release':
+        ensure => present,
+        before => Package['tinyproxy'],
+      }
+    }
+    default:{
+      fail("Operating system #{operatingsystem} is not supported.")
+    }
+  }
+  # Install tiny proxy package
+  package { 'tinyproxy':
+    ensure => present,
+  }
+
+  # Create config file
+  file { "$confdir":
+    ensure  => file,
+    content => template('proxy_setup/tinyproxy.erb'),
+    require => Package['tinyproxy'],
+    notify  => Service['tinyproxy'],
+  }
+
+  # Create service
+  service {'tinyproxy':
+    ensure => running,
+  }
+}
