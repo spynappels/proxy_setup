eb64debe839a3ec0b671dbc63e52670b2b1493f0
Stefan Pijnappels <stefan@puppet.com>
2016-09-01 11:50:07 +0100

    app template support

---
+++ manifests/new.init.pp
@@ -0,0 +1,44 @@
+# This is the init.pp for the proxy_setup module
+
+class proxy_setup (
+  # Create parameter and default value for port
+  $port = '8888',
+){
+  # Check for OS family to verify it's Linux and set the confdir
+  case $::osfamily {
+    'debian':{
+      $confdir = '/etc/tinyproxy.conf'
+    }
+    'redhat':{
+      # In RedHat we also need to install the Epel repo
+      $confdir = '/etc/tinyproxy/tinyproxy.conf'
+      package {'epel-release':
+        ensure => present,
+        before => Package['tinyproxy'],
+      }
+    }
+    default:{
+      fail("Operating system #{operatingsystem} is not supported.")
+    }
+  }
+  # Install tiny proxy package
+  package { 'tinyproxy':
+    ensure => present,
+  }
+
+  # Create config file
+  file { "$confdir":
+    ensure  => file,
+    content => epp('proxy_setup/tinyproxy.epp', {
+      'port' => $port,
+      }),
+    require => Package['tinyproxy'],
+    notify  => Service['tinyproxy'],
+  }
+
+  # Create service
+  service {'tinyproxy':
+    ensure => running,
+  }
+  notify {"The proxy is running on ${::ipaddress_eth1} on port ${port}.":}
+}
